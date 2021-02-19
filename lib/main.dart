f936565850208cc656ea091b551b301a7893c63f
M7mdisk <M7md.k.isk@gmail.com>
2021-02-20 01:34:34 +0300

    Seperated pages and organized file structure.

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:printed/pages/login.dart';
+import 'package:printed/pages/register.dart';
 
 void main() {
   runApp(MyApp());
@@ -30,105 +31,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Center(
-        child: Container(
-          color: Colors.white,
-          child: Padding(
-            padding: const EdgeInsets.all(20.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                SizedBox(
-                  height: 155.0,
-                  child: Text.rich(
-                    TextSpan(
-                        style: TextStyle(
-                            color: Colors.black,
-                            fontSize: 65,
-                            fontWeight: FontWeight.w600),
-                        children: [
-                          TextSpan(text: "Welcome to "),
-                          TextSpan(
-                              text: "Printed.",
-                              style: TextStyle(
-                                  fontWeight: FontWeight.w900,
-                                  foreground: Paint()
-                                    ..shader = LinearGradient(
-                                      colors: <Color>[
-                                        Colors.purple[900],
-                                        Colors.purple[600]
-                                      ],
-                                    ).createShader(
-                                        Rect.fromLTWH(0.0, 0.0, 200.0, 70.0))))
-                        ]),
-                  ),
-                ),
-                SizedBox(height: 45.0),
-                TextField(
-                  obscureText: false,
-                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
-                  decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      hintText: "Username",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0))),
-                ),
-                SizedBox(height: 25.0),
-                TextField(
-                  obscureText: true,
-                  style: TextStyle(fontFamily: 'Montserrat', fontSize: 20.0),
-                  decoration: InputDecoration(
-                      contentPadding:
-                          EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                      hintText: "Password",
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(32.0))),
-                ),
-                SizedBox(
-                  height: 35.0,
-                ),
-                Material(
-                  elevation: 5.0,
-                  borderRadius: BorderRadius.circular(30.0),
-                  color: Colors.purple[800],
-                  child: MaterialButton(
-                    minWidth: MediaQuery.of(context).size.width,
-                    padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-                    onPressed: () {},
-                    child: Text("Login",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            fontFamily: 'Montserrat',
-                            fontSize: 20.0,
-                            color: Colors.white,
-                            fontWeight: FontWeight.bold)),
-                  ),
-                ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Text.rich(
-                  TextSpan(
-                    style: TextStyle(fontSize: 15),
-                    children: <TextSpan>[
-                      TextSpan(text: "Don't have an account?"),
-                      TextSpan(
-                          text: ' Register.',
-                          style: TextStyle(
-                              color: Colors.blue, fontWeight: FontWeight.bold),
-                          recognizer: TapGestureRecognizer()
-                            ..onTap = () => print('click')),
-                    ],
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ),
-      ),
-    );
+    return Login();
   }
 }
